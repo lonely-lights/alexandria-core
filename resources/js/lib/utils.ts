43572965c20d5bfe6167c59d7ec43cc8a80d165f
Andrew K. Hartley <andrew@lonelylights.com>
2026-06-15 17:43:15 -0400

    chore(stage-9): remove legacy date utility

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -7,19 +7,6 @@ export function cn(...inputs: ClassValue[]): string {
     return clsx(inputs);
 }
 
-export function formatDate(date: string | null, options?: Intl.DateTimeFormatOptions): string {
-    if (!date) {
-        return '';
-    }
-
-    return new Intl.DateTimeFormat('en-US', {
-        month: 'short',
-        day: 'numeric',
-        year: 'numeric',
-        ...options,
-    }).format(new Date(date));
-}
-
 export function truncate(str: string, length: number): string {
     if (str.length <= length) {
         return str;
